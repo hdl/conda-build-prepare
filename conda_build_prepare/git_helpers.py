17e71e4ce25c0b82ac0bec16557b6fe8f36d1ff2
Johan Euphrosine <proppy@google.com>
2022-12-23 21:39:48 +0900

    test: add git_helpers unit tests

--- conda_build_prepare/git_helpers.py
+++ conda_build_prepare/git_helpers.py
@@ -145,11 +145,11 @@ def _call_custom_git_cmd(git_repo, cmd_string, check=True, quiet=False):
     return stdout.strip()
 
 
-def get_latest_tag(git_repo):
+def get_first_reachable_tag(git_repo):
     try:
-        tag_commit = _call_custom_git_cmd(git_repo, f'rev-list --tags -1')
-        tag_refname = _call_custom_git_cmd(git_repo, f'tag --points-at {tag_commit}')
-        return tag_refname
+        # list reachable tag sorted by commit date
+        tags = _call_custom_git_cmd(git_repo, 'tag --merged').splitlines()
+        return tags[-1]
     except subprocess.CalledProcessError:
         return None
 
@@ -233,7 +233,7 @@ def git_rewrite_tags(git_repo):
     print(f'\nRewriting tags in "{os.path.abspath(git_repo)}"...\n')
 
     while True:
-        tag = get_latest_tag(git_repo)
+        tag = get_first_reachable_tag(git_repo)
         if tag is None:
             # Add '0.0' tag on initial commit and skip rewriting.
             git_add_initial_tag(git_repo)
